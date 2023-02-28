abad880074b8a8df6661e583d50db83abedba575
Dmitrii Zverev <104170542+TimBearWeiss@users.noreply.github.com>
2023-02-28 15:36:16 +0300

    Delete ModalOverlay.jsx

--- src/components/ModalOverlay/ModalOverlay.jsx
+++
@@ -1,7 +0,0 @@
-import style from "./ModalOverlay.module.css";
-
-const ModalOverlay = ({ close }) => {
-  return <div className={style.overlay} onClick={close}></div>;
-};
-
-export default ModalOverlay;
